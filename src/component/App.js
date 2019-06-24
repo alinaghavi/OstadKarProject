34a5842b89b632a76d7c32a3c988fbd5557d8474
naghavi.ali <naghavi.ali@gmail.com>
2019-06-24 12:12:24 +0430

    Materialize and Scss compiler added

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../scss/App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import Materialize from 'materialize-css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+
+    componentDidMount() {
+        // get a reference to the materialize after the component has mounted
+        Materialize.AutoInit();
+    }
+
+    render() {
+        return (
+            <div className="App">
+            </div>
+        )
+    }
 }
 
 export default App;
